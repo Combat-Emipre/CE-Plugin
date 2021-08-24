1e6b484206c9a442a5aa93efd00215510b29c9fa
Under97 <66751151+Underplayer97@users.noreply.github.com>
2021-08-24 13:22:20 -0400

    Create leave.java

---
+++ src/com/github/underplayer97/P11C/commands/leave.java
@@ -0,0 +1,45 @@
+package com.github.underplayer97.P11C.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.underplayer97.P11C.Main;
+import com.github.underplayer97.P11C.utils.Utils;
+
+public class HubCmd implements CommandExecutor{
+	
+	private Main plugin; 
+	
+	public HubCmd(Main plugin) {
+		this.plugin = plugin;
+		
+		plugin.getCommand("leave").setExecutor(this);
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {		
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(Utils.chat(plugin.getConfig().getString("console_error_message")));
+			return true;
+		}
+		
+		Player p = (Player) sender;
+		
+		if (p.hasPermission("P11C.Leave")) {
+			 {
+				 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "server lobby");
+				return true;
+		}}
+				
+
+				
+			 else {
+				p.sendMessage(Utils.chat(plugin.getConfig().getString("no_perm_error")));
+			}
+				return false;
+	}
+	
+}
