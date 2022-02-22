5e303a6e2c2f7e9fe33ab62a43f4b8a18831f1d0
Under97 <66751151+Underplayer97@users.noreply.github.com>
2022-02-22 12:45:43 -0500

    Update and rename src/com/github/underplayer97/P11C/commands/HubCmd.java to src/com/github/underplayer97/CE/commands/HubCmd.java

--- src/com/github/underplayer97/CE/commands/HubCmd.java
+++ src/com/github/underplayer97/CE/commands/HubCmd.java
@@ -1,4 +1,4 @@
-package com.github.underplayer97.P11C.commands;
+package com.github.underplayer97.CE.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -6,8 +6,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.github.underplayer97.P11C.Main;
-import com.github.underplayer97.P11C.utils.Utils;
+import com.github.underplayer97.CE.Main;
+import com.github.underplayer97.CE.utils.Utils;
 
 public class HubCmd implements CommandExecutor{
 	
@@ -28,7 +28,7 @@ public class HubCmd implements CommandExecutor{
 		
 		Player p = (Player) sender;
 		
-		if (p.hasPermission("P11C.Hub")) {
+		if (p.hasPermission("CE.Hub")) {
 			 {
 				 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "server lobby");
 				return true;
